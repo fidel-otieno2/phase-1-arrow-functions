108e3e16ed2b0e2786f6dc780506e681cdd1f42e
fidel-otieno2 <fidelmartins05@gmail.com>
2025-06-11 07:37:39 +0300

    jimmy

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function () {
+    return (2000 / 100);
+}
+
+const square = parameter1 => {
+    return(parameter1 * parameter1);
+}
+
+const add = (par1, par2) => {
+    return(par1 + par2);
+}
